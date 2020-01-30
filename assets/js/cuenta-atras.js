ed4aeaea33dc36734d68a8cafa916d77e3881a7a
Samuel <smltrs0@gmail.com>
2020-01-30 15:49:53 -0430

    original

---
+++ assets/js/cuenta-atras.js
@@ -0,0 +1,73 @@
+const getTime = dateTo => {
+    let now = new Date(),
+        time = (new Date(dateTo) - now + 1000) / 1000,
+        seconds = ('0' + Math.floor(time % 60)).slice(-2),
+        minutes = ('0' + Math.floor(time / 60 % 60)).slice(-2),
+        hours = ('0' + Math.floor(time / 3600 % 24)).slice(-2),
+        days = Math.floor(time / (3600 * 24));
+ 
+    return {
+        seconds,
+        minutes,
+        hours,
+        days,
+        time
+    }
+};
+ 
+const countdown = (dateTo, element) => {
+    const item = document.getElementById(element);
+ 
+    const timerUpdate = setInterval( () => {
+        let currenTime = getTime(dateTo);
+        item.innerHTML = `
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.days}
+                        </div>
+                        <div class="concept">
+                            Días
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.hours}
+                        </div>
+                        <div class="concept">
+                            Horas
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.minutes}
+                        </div>
+                        <div class="concept">
+                            Minutos
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="countdown-container">
+                        <div class="number">
+                            ${currenTime.seconds}
+                        </div>
+                        <div class="concept">
+                            Segundos
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+ 
+        if (currenTime.time <= 1) {
+            clearInterval(timerUpdate);
+            alert('Fin de la cuenta '+ element);
+        }
+ 
+    }, 1000);
+};
